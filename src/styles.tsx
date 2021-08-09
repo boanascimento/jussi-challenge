a69b11c322d0fa13297e5bc5ca042a5ce5255bf4
Boanerges Souza do Nascimento <boanerges.nascimento@compasso.com.br>
2021-08-09 20:34:13 -0300

    Incluindo dados simples

--- src/styles.tsx
+++ src/styles.tsx
@@ -1,9 +1,10 @@
 export enum Colors {
   greenJussi = '#03FFA5',
   greyLight = '#F2F2F2',
-  pinkJussi = '#EE0E7D'
+  pinkJussi = '#EE0E7D',
+  greyDark = '#5B5B5B',
 }
 
 export enum Default {
-  transition = '0.3s'
+  transition = '0.3s',
 }
